71b35f5b630a3706dab0fbebca1b3a50880f060e
Ernesto <ernesto.toshiyuki@yahoo.com>
2023-03-17 19:36:56 +0000

    Added the second tag

--- main.tf
+++ main.tf
@@ -128,7 +128,8 @@ resource "aws_instance" "hashicat" {
 
   tags = {
     Name = "${var.prefix}-hashicat-instance"
-    Department = "true"
+    Department = "devops"
+    Billable = "true"
   }
 }
 
